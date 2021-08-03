29f31ed1c134c70336e69a63569a2f3f1b848f42
mangonob <mangonob@qq.com>
2021-08-03 22:50:23 +0800

    feat: Simple Rotate

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,23 @@
 import 'package:charts_flutter/flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_demo/auto_rotated.dart';
 import 'package:flutter_demo/only_text.dart';
 import 'package:flutter_demo/routes/routes.dart';
 import 'package:flutter_demo/some_stuff.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 void main() {
   StyleFactory.style = CustomChartStyle();
 
   runApp(
-    AutoRotated(
-      child: MaterialApp(
-        routes: {
-          Routes.first: (_) => OnlyText(text: "first"),
-          Routes.second: (_) => OnlyText(text: "second"),
-          Routes.third: (_) => OnlyText(text: "third"),
-        },
-        title: "My app",
-        home: SomeStuff(),
-      ),
+    MaterialApp(
+      routes: {
+        Routes.first: (_) => OnlyText(text: "first"),
+        Routes.second: (_) => OnlyText(text: "second"),
+        Routes.third: (_) => OnlyText(text: "third"),
+      },
+      title: "My app",
+      home: SomeStuff(),
+      builder: EasyLoading.init(),
     ),
   );
 }
--- lib/some_stuff.dart
+++ lib/some_stuff.dart
@@ -1,93 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_demo/auto_rotated.dart';
+import 'package:flutter_demo/widgets/popover.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
-class SomeStuff extends StatefulWidget {
-  @override
-  _SomeStuffState createState() => _SomeStuffState();
-}
-
-class _SomeStuffState extends State<SomeStuff> {
-  double _opacity = 1;
-
+class SomeStuff extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: AnimatedOpacity(
-        duration: Duration(milliseconds: 250),
-        opacity: _opacity,
-        child: Scaffold(
-          body: GestureDetector(
-            onDoubleTap: () => setState(() => _opacity = 0),
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: List.generate(100, (index) {
-                return Container(
-                  color: [Colors.red, Colors.green, Colors.blue][index % 3],
-                  child: ListTile(),
-                );
-              }),
-            ),
+      child: Scaffold(
+        body: Center(
+          child: TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (ctx) => Others()),
+              );
+            },
+            child: Text("Push"),
           ),
         ),
-        onEnd: () async {
-          if (_opacity == 0) {
-            AutoRotatedNotification(DeviceOrientation.landscapeRight)
-                .dispatch(context);
-            await Navigator.push(context,
-                PageRouteBuilder(pageBuilder: (ctx, a1, a2) => Others()));
-            AutoRotatedNotification(DeviceOrientation.portraitUp)
-                .dispatch(context);
-            setState(() => _opacity = 1);
-          }
-        },
       ),
     );
   }
 }
 
-class Others extends StatefulWidget {
-  @override
-  _OthersState createState() => _OthersState();
-}
-
-class _OthersState extends State<Others> {
-  double _opacity = 0;
-
-  @override
-  void initState() {
-    super.initState();
-
-    setState(() => _opacity = 1);
-  }
-
+class Others extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: AnimatedOpacity(
-        duration: Duration(milliseconds: 250),
-        opacity: _opacity,
-        child: Scaffold(
-          body: GestureDetector(
-            onDoubleTap: () => setState(() => _opacity = 0),
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: List.generate(100, (index) {
-                return Container(
-                  color: [Colors.red, Colors.green, Colors.blue][index % 3],
-                  child: ListTile(),
-                );
-              }),
+    return RotatedBox(
+      quarterTurns: 1,
+      child: FlutterEasyLoading(
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Container(
+            color: Colors.white,
+            child: Scaffold(
+              body: GestureDetector(
+                onDoubleTap: () {
+                  Navigator.pop(context);
+                },
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      DropdownButton(
+                        onChanged: (v) {},
+                        items: [
+                          DropdownMenuItem(child: Text("A"), value: 0),
+                          DropdownMenuItem(child: Text("A"), value: 1),
+                          DropdownMenuItem(child: Text("A"), value: 2),
+                        ],
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          EasyLoading.showInfo("Some Info");
+                        },
+                        child: Text("EasyLoading"),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ),
           ),
         ),
-        onEnd: () {
-          if (_opacity == 0) {
-            Navigator.pop(context);
-          }
-        },
       ),
     );
   }
