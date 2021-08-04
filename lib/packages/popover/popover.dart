f13c1bd963d125173776b817c4742b4c3ea5fb63
mangonob <mangonob@qq.com>
2021-08-04 21:31:49 +0800

    chore: Clean up

--- lib/packages/popover/popover.dart
+++
@@ -1,2 +0,0 @@
-export 'src/popover.dart';
-export 'src/popover_direction.dart';
--- lib/some_stuff.dart
+++ lib/some_stuff.dart
@@ -1,123 +1,12 @@
 import 'package:app_popup_menu/app_popup_menu.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_demo/widgets/popover.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class SomeStuff extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (ctx) => Others()),
-                  );
-                },
-                child: Text("Push"),
-              ),
-              Popover(
-                child: Text("Popover"),
-                popoverBuilder: (ctx) => Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Container(
-                      width: 100,
-                      height: 50,
-                      color: Colors.red,
-                    ),
-                    Container(
-                      width: 100,
-                      height: 50,
-                      color: Colors.green,
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class Others extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return RotatedBox(
-      quarterTurns: 1,
-      child: FlutterEasyLoading(
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: Container(
-            color: Colors.white,
-            child: Scaffold(
-              body: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Popover(
-                      child: Text("Popover"),
-                      popoverBuilder: (ctx) => Column(
-                        children: [
-                          Container(
-                            width: 100,
-                            height: 50,
-                            color: Colors.red,
-                          ),
-                          Container(
-                            width: 100,
-                            height: 50,
-                            color: Colors.green,
-                          ),
-                        ],
-                      ),
-                    ),
-                    AppPopupMenu(
-                      initialValue: 2,
-                      child: Text("Dropdown"),
-                      menuItems: [
-                        PopupMenuItem(
-                          child: Text("A"),
-                          value: 1,
-                        ),
-                        PopupMenuItem(
-                          child: Text("A"),
-                          value: 2,
-                        ),
-                      ],
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      padding: EdgeInsets.zero,
-                    ),
-                    DropdownButton(
-                      onChanged: (v) {},
-                      items: [
-                        DropdownMenuItem(child: Text("A"), value: 0),
-                        DropdownMenuItem(child: Text("A"), value: 1),
-                        DropdownMenuItem(child: Text("A"), value: 2),
-                      ],
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        EasyLoading.showInfo("Some Info");
-                      },
-                      child: Text("EasyLoading"),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
+    return Scaffold(
+      body: Center(
+        child: Text("Some Stuff"),
       ),
     );
   }
